646fe9cf982cc6e9974f1a05fa162afd8be63a0a
Sora Arakawa <mail@arkw.net>
2025-12-12 22:54:24 +0900

    Add search function

--- main.cpp
+++ main.cpp
@@ -2,8 +2,10 @@
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Box.H>
 #include <chrono>
+#include <cstdio>
 #include <ctime>
 #include <iomanip>
+#include <iostream>
 #include <sstream>
 #include <string>
 
@@ -29,9 +31,41 @@ std::string timestamp() {
     return oss.str();
 }
 
+std::string search(FILE* pipe, const std::string& handle_key, const std::string& type_key, const std::string& field_key) {
+  if (!pipe) {
+    return "";
+  }
+  std::string line;
+  char buffer[256];
+  bool in_target_handle = false;
+  while (fgets(buffer, sizeof(buffer), pipe)) {
+      line = buffer;
+      if (line.find(handle_key) != std::string::npos && line.find(type_key) != std::string::npos) {
+        in_target_handle = true;
+        continue;
+      }
+      if (in_target_handle && line.find("Handle ") != std::string::npos) {
+        break;
+      }
+      if (in_target_handle && line.find(field_key) != std::string::npos) {
+        std::string value = line.substr(line.find(":") + 1);
+        while (!value.empty() && (value[0] == ' ' || value[0] == '\t')) {
+          value.erase(0, 1);
+        }
+        while (!value.empty() && (value.back() == '\n' || value.back() == '\r')) {
+          value.pop_back();
+        }
+        return value;
+      }
+  }
+  return "";
+}
+
 int main(int argc, char **argv) {
+  std::string temp;
   Fl_Window *window = new Fl_Window(WINDOW_WIDTH, WINDOW_HEIGHT, "Project Seesaw");
   window->color(FL_WHITE);
+
   Fl_Box *box_software_details = new Fl_Box(20, 10, 200, 40, "Software Details");
   box_software_details->align(FL_ALIGN_LEFT | FL_ALIGN_INSIDE);
   box_software_details->labelsize(20);
@@ -44,14 +78,30 @@ int main(int argc, char **argv) {
   box_software_date_right->align(FL_ALIGN_LEFT | FL_ALIGN_INSIDE);
   box_software_date_right->labelsize(14);
 
-  std::string version = PRODUCT_NAME + ", " + PRODUCT_VERSION;
+  temp = PRODUCT_NAME + ", " + PRODUCT_VERSION;
   Fl_Box *box_software_version_left = new Fl_Box(20, 70, 200, 20, "Software Version:");
   box_software_version_left->align(FL_ALIGN_LEFT | FL_ALIGN_INSIDE);
   box_software_version_left->labelsize(14);
-  Fl_Box *box_software_version_right = new Fl_Box(220, 70, 200, 20, version.c_str());
+  Fl_Box *box_software_version_right = new Fl_Box(220, 70, 200, 20);
   box_software_version_right->align(FL_ALIGN_LEFT | FL_ALIGN_INSIDE);
   box_software_version_right->labelsize(14);
+  box_software_version_right->copy_label(temp.c_str());
 
+  Fl_Box *box_hardware_details = new Fl_Box(20, 100, 200, 40, "Hardware Details");
+  box_hardware_details->align(FL_ALIGN_LEFT | FL_ALIGN_INSIDE);
+  box_hardware_details->labelsize(20);
+
+  FILE* pipe = popen("sudo dmidecode", "r");
+  temp = search(pipe, "Handle 0x000C", "DMI type 1", "Manufacturer:");
+  Fl_Box *box_hardware_manufacturer_left = new Fl_Box(20, 140, 200, 20, "Manufacturer:");
+  box_hardware_manufacturer_left->align(FL_ALIGN_LEFT | FL_ALIGN_INSIDE);
+  box_hardware_manufacturer_left->labelsize(14);
+  Fl_Box *box_hardware_manufacturer_right = new Fl_Box(220, 140, 200, 20);
+  box_hardware_manufacturer_right->align(FL_ALIGN_LEFT | FL_ALIGN_INSIDE);
+  box_hardware_manufacturer_right->labelsize(14);
+  box_hardware_manufacturer_right->copy_label(temp.c_str());
+
+  pclose(pipe);
   window->end();
   window->show(argc, argv);
   return Fl::run();
